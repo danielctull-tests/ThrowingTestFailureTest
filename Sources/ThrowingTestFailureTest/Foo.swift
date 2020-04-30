b798f9c0b1dd9018485e6a4017a4406945e80b39
Daniel Tull <dt@danieltull.co.uk>
2020-04-30 13:24:46 +0100

    Show the XCTUnwrap case

--- Sources/ThrowingTestFailureTest/Foo.swift
+++ Sources/ThrowingTestFailureTest/Foo.swift
@@ -1,8 +1,13 @@
 
 public struct Foo {
     public let string: String
-    public init(string: String) throws {
+
+    public init(throwing: String) throws {
         struct FooFailure: Error {}
         throw FooFailure()
+    }
+
+    public init?(optional: String) {
+        return nil
     }
 }
--- Tests/ThrowingTestFailureTestTests/FooTests.swift
+++ Tests/ThrowingTestFailureTestTests/FooTests.swift
@@ -10,13 +10,18 @@ func getString() throws -> String {
 final class FooTests: XCTestCase {
 
     func testFoo() throws {
-        let foo = try Foo(string: "hello")
+        let foo = try Foo(throwing: "hello")
         XCTAssertEqual(foo.string, "hello")
     }
 
     func testFooWithSetup() throws {
         let string = try getString()
-        let foo = try Foo(string: string)
+        let foo = try Foo(throwing: string)
+        XCTAssertEqual(foo.string, "hello")
+    }
+
+    func testUnwrap() throws {
+        let foo = try XCTUnwrap(Foo(optional: "hello"))
         XCTAssertEqual(foo.string, "hello")
     }
 }
